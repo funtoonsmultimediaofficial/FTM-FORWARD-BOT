a411dec9a99feb5ea34b3ccf056751634d797deb
funtoonsmultimediaofficial <funtoonsmultimedia@gmail.com>
2024-08-25 06:25:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "13786499")
+    API_HASH = os.environ.get("API_HASH", "429c3423fba4cac53c02b8da777e93a8")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6659796121:AAGi_qBMIy_jYgjWcWniYtNEQr0LUMGj06w") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://mrindianlekhak:ftm@cluster0.3ruvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7113960085').split()]
 
 
 class temp(object): 
